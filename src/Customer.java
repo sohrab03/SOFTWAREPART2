7b213266675eb800ba20ddc588a8b56168035854
sohrab03 <sohrab12341234@gmail.com>
2024-12-25 22:04:22 +0000

    Created Customer Class

---
+++ src/Customer.java
@@ -0,0 +1,28 @@
+public class Customer {
+    private int sequenceNumber;
+    private String name;
+    private String parcelId;
+
+    public Customer(int sequenceNumber, String name, String parcelId) {
+        this.sequenceNumber = sequenceNumber;
+        this.name = name;
+        this.parcelId = parcelId;
+    }
+
+    public int getSequenceNumber() {
+        return sequenceNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getParcelId() {
+        return parcelId;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer [SequenceNumber=" + sequenceNumber + ", Name=" + name + ", ParcelID=" + parcelId + "]";
+    }
+}
